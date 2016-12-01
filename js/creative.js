c22b3baefa1efa70fb139b1345c7747e7f23d3bf
Hristina Klincarova <hristina.klincarova@gmail.com>
2016-12-01 01:09:09 +0100

    final en version

--- js/creative.js
+++ js/creative.js
@@ -1,11 +1,18 @@
 (function($) {
     "use strict"; // Start of use strict
-
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+	
+	$('#nagore').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top +250)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
